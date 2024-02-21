349b75745469b3f202513cfd12ad3b726bb2cebe
Rafi <tahmidulrafi13@gmail.com>
2024-02-21 21:50:49 +0600

    google and email verification added from firebase

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -1,12 +1,61 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
+import app from "../firebase/firebase.config";
+import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 
 export const AuthContext = createContext(null);
+const auth= getAuth(app)
+const AuthProvider = ({children}) => {
+    const [user, setUser]= useState(null);
+    const [loading, setLoading] = useState(true);
+    const googleProvider = new GoogleAuthProvider();
 
-const AuthProvider = () => {
+    const register=(email,password)=>{
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth,email,password);
+    }
+    const login=(email,password)=>{
+        setLoading(true);
+        return signInWithEmailAndPassword(auth,email,password);
+    }
+    const googleSignIn=()=>{
+        return signInWithPopup(auth, googleProvider);
+    }
+    const logout=()=>{
+        return signOut(auth);
+    }
+
+    const profileUpdate=(name, imageURL)=>{
+        setLoading(true);
+        return updateProfile(auth.currentUser,{
+            displayName: name,
+            photoURL: imageURL,
+        })
+    }
+    useEffect(()=>{
+        const unSubscribe = onAuthStateChanged(auth, currentUser=>{
+            setUser(currentUser);
+            console.log('current user ',currentUser);
+            setLoading(false)
+        })
+        return ()=>{
+            return unSubscribe();
+        }
+    },[])
+
+    const authInfo={
+        user,
+        loading,
+        setLoading,
+        register,
+        login,
+        googleSignIn,
+        logout,
+        profileUpdate
+    }
     return (
-        <div>
-            
-        </div>
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
     );
 };
 
